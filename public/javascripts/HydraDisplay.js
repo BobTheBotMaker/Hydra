e33cb3f58be84fad709e49ccccaf78243998e85e
BobTheBotMaker <andre@hudat.com>
2013-10-24 22:04:43 -0400

    Started display styling

---
+++ public/javascripts/HydraDisplay.js
@@ -0,0 +1,39 @@
+function HydraVoltDisplay(canvasID) {
+  
+  var display = new SegmentDisplay(canvasID);
+  display.pattern         = "##.##";
+  display.displayAngle    = 5.5;
+  display.digitHeight     = 3.5;
+  display.digitWidth      = 2;
+  display.digitDistance   = 1.3;
+  display.segmentWidth    = 0.2;
+  display.segmentDistance = 0.3;
+  display.segmentCount    = 7;
+  display.cornerType      = 3;
+  display.colorOn         = "#ff2c0f";
+  display.colorOff        = "#331605";
+  display.draw();
+  
+  return(display);
+
+}
+
+function HydraAmpDisplay(canvasID) {
+  
+  var display = new SegmentDisplay(canvasID);
+  display.pattern         = "##.##";
+  display.displayAngle    = 5.5;
+  display.digitHeight     = 3.5;
+  display.digitWidth      = 2;
+  display.digitDistance   = 1.3;
+  display.segmentWidth    = 0.2;
+  display.segmentDistance = 0.3;
+  display.segmentCount    = 7;
+  display.cornerType      = 3;
+  display.colorOn         = "#24dd22";
+  display.colorOff        = "#1b2405";
+  display.draw();
+  
+  return(display);
+
+}
