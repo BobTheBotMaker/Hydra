99c018edc69b52ef29164815ff83569b0d43aeb2
BobTheBotMaker <andre@hudat.com>
2013-10-25 13:45:59 -0400

    DRY up display creation JS

--- public/javascripts/HydraDisplay.js
+++ public/javascripts/HydraDisplay.js
@@ -1,4 +1,4 @@
-function HydraVoltDisplay(canvasID) {
+function HydraDisplay(canvasID, colorOn, colorOff) {
   
   var display = new SegmentDisplay(canvasID);
   display.pattern         = "##.##";
@@ -10,36 +10,17 @@ function HydraVoltDisplay(canvasID) {
   display.segmentDistance = 0.3;
   display.segmentCount    = 7;
   display.cornerType      = 3;
-  display.colorOn         = "#ff2c0f";
-  display.colorOff        = "#331605";
+  display.colorOn         = colorOn;
+  display.colorOff        = colorOff;
   display.draw();
   
   return(display);
 
 }
 
-function HydraAmpDisplay(canvasID) {
-  
-  var display = new SegmentDisplay(canvasID);
-  display.pattern         = "##.##";
-  display.displayAngle    = 5.5;
-  display.digitHeight     = 3.5;
-  display.digitWidth      = 2;
-  display.digitDistance   = 1.3;
-  display.segmentWidth    = 0.2;
-  display.segmentDistance = 0.3;
-  display.segmentCount    = 7;
-  display.cornerType      = 3;
-  display.colorOn         = "#24dd22";
-  display.colorOff        = "#1b2405";
-  display.draw();
-  
-  return(display);
-
-}
-
-function updatdeDisplay(display) {
+function updateDisplay(display) {
   $.get("val.txt", function(data,status) {
     display.setValue(data);
+    window.setTimeout(updateDisplay, 100, display);
   });
 }
